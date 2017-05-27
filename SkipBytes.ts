de4301ac7d034eccad1c865e508cfa8dd05cc027
Alicia Boya García <ntrrgc@gmail.com>
2017-05-28 01:40:06 +0200

    Add comment to SkipBytes.ts

--- SkipBytes.ts
+++ SkipBytes.ts
@@ -1,12 +1,14 @@
 import * as stream from "stream";
 
+// Adapted from stream-skip
+// https://github.com/Ivshti/stream-skip
 export class SkipBytes extends stream.Transform {
     constructor(private _toSkip: number) {
         super({})
     }
 
     _transform(chunk, enc, cb) {
-        if (this._toSkip == 0) this.push(chunk)
+        if (this._toSkip == 0) this.push(chunk);
         else if (this._toSkip > chunk.length) {
             this._toSkip -= chunk.length;
         } else {
