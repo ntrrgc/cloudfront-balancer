f639f32a297882f2e4654d215642e9f7a8cfb8c2
Alicia Boya García <ntrrgc@gmail.com>
2017-05-28 03:28:46 +0200

    Restart service remotely

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rsync ./ --exclude node_modules -r pi@192.168.0.230:cloudfront-balancer/
+set -eu
+rsync -P ./ --exclude node_modules -r pi@192.168.0.230:cloudfront-balancer/
+ssh pi@192.168.0.230 sudo systemctl restart cloudfront-balancer
